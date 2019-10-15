90d537f7513501cecfa9094234906cb6da6c6e41
lyg <baayoo93@gmail.com>
2019-10-15 23:01:35 +0900

    Make basic server

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var server = http.createServer(function(request,response){
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.end('Hello node.js!!');
+});
+server.listen(8080, function(){ 
+    console.log('Server is running...');
+});
+
