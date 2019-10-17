0703b1b8892de953255137e6013c954d1cd10cf3
lyg <baayoo93@gmail.com>
2019-10-17 23:51:55 +0900

    Make Imageslider

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var fs = require('fs');
+
 var server = http.createServer(function(request,response){
     response.writeHead(200,{'Content-Type':'text/html'});
-    response.end('Hello node.js!!');
+    fs.createReadStream("./index.html").pipe(response);
 });
 server.listen(8080, function(){ 
     console.log('Server is running...');
