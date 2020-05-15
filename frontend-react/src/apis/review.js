d30014f59b9ad3f4602063b40675bbc7329aaec3
younggyulee <baayoo93@naver.com>
2020-05-15 17:39:51 +0900

    add gitignore

--- frontend-react/src/apis/review.js
+++ frontend-react/src/apis/review.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import URL from './url';
 
 export const getReviewList = async (id) => {
-    console.log(id,'dasds');
     const { data } = await axios.get(`${URL}/reviewlist/${id}`, { withCredentials: true });
     return data;
 }
--- frontend-react/src/apis/url.js
+++ frontend-react/src/apis/url.js
@@ -1,4 +1,4 @@
-// const URL = 'http://ec2-15-164-220-31.ap-northeast-2.compute.amazonaws.com:3000/'
-const URL = 'http://localhost:3000';
+const URL = 'http://ec2-13-125-74-163.ap-northeast-2.compute.amazonaws.com:3000'
+// const URL = 'http://localhost:3000';
 
 export default URL;
