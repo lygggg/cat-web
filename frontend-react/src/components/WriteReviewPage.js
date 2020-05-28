38b6e4e5eaa2046084aec940426e0a5dd879777a
younggyulee <baayoo93@naver.com>
2020-05-28 18:35:25 +0900

    test swal

--- frontend-react/src/components/WriteReviewPage.js
+++ frontend-react/src/components/WriteReviewPage.js
@@ -31,6 +31,33 @@ function WriteReviewPage() {
   };
 
   const sendReview = async () => {
+    let timerInterval
+Swal.fire({
+  title: 'Auto close alert!',
+  html: 'I will close in <b></b> milliseconds.',
+  timer: 2000,
+  timerProgressBar: true,
+  onBeforeOpen: () => {
+    Swal.showLoading()
+    timerInterval = setInterval(() => {
+      const content = Swal.getContent()
+      if (content) {
+        const b = content.querySelector('b')
+        if (b) {
+          b.textContent = Swal.getTimerLeft()
+        }
+      }
+    }, 100)
+  },
+  onClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
     const formData = new FormData();
     formData.append("myfile", reviewPhoto);
     formData.append("productId", ReviewProduct.id);
@@ -38,52 +65,25 @@ function WriteReviewPage() {
     formData.append("reviewText", reviewText);
     formData.append("productTitle", ReviewProduct.title);
     const review = await postReview(formData);
-
-    Swal.fire({
-      timerProgressBar: true,
-      title: "Wating...",
-      text: "업로드중입니다.",
-    });
-
-    let timerInterval;
-    Swal.fire({
-      title: "Auto close alert!",
-      html: "I will close in <b></b> milliseconds.",
-      timer: 2000,
-      timerProgressBar: true,
-      onBeforeOpen: () => {
-        Swal.showLoading();
-        timerInterval = setInterval(() => {
-          const content = Swal.getContent();
-          if (content) {
-            const b = content.querySelector("b");
-            if (b) {
-              b.textContent = Swal.getTimerLeft();
-            }
-          }
-        }, 100);
-      },
-      onClose: () => {
-        clearInterval(timerInterval);
-      },
-    }).then((result) => {
-      if (review.statusText === "OK") {
-        Swal.fire("성공", "리뷰 등록이 완료되었습니다!", "success").then(
-          (result) => {
-            if (result.value) {
-              history.push("/review");
-            }
-          }
-        );
-      } else {
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "Something went wrong!",
-          footer: "<a href>Why do I have this issue?</a>",
-        });
-      }
-    });
+    if (review.statusText === 'OK') {
+      Swal.fire(
+        '성공',
+        '리뷰 등록이 완료되었습니다!',
+        'success'
+      ).then((result) => {
+        if (result.value) {
+          history.push('/review');
+        }
+      })
+    }
+    else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: '<a href>Why do I have this issue?</a>'
+      })
+    }
   };
 
   useEffect(() => {
