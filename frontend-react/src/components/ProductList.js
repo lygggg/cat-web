da1c130529531ae694b4b150bf56d1159fd8425d
younggyulee <baayoo93@naver.com>
2022-01-22 19:31:44 +0900

    deploy

--- frontend-react/src/components/ProductList.js
+++ frontend-react/src/components/ProductList.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
-import store from '../stores/ProductStore';
-import Product from './Product';
-import Pagination from './Pagination';
+import store from "../stores/ProductStore";
+import Product from "./Product";
+import Pagination from "./Pagination";
 
+import { getSliceProducts } from "../service/productService";
 
-import { getSliceProducts } from '../service/productService';
-
-import { Button } from '../lib/Button';
-import ItemListDiv, { Div, H1, MainDiv } from '../lib/Grid';
+import { Button } from "../lib/Button";
+import ItemListDiv, { Div, H1, MainDiv } from "../lib/Grid";
 
 const MAX_ITEM = 9;
 
@@ -58,36 +57,32 @@ function ProductList() {
   });
 
   return (
-    <MainDiv>  
-        <Div>
-         <H1>{categoryName}</H1>
-            <div>
-                <Button onClick={() => lowprice(productList)}>가격 낮은순</Button>
-                <Button onClick={() => highprice(productList)}>가격 높은순</Button>
-            </div>
+    <MainDiv>
+      <Div>
+        <H1>{categoryName}</H1>
+        <div>
+          <Button onClick={() => lowprice(productList)}>가격 낮은순</Button>
+          <Button onClick={() => highprice(productList)}>가격 높은순</Button>
+        </div>
 
-            <ItemListDiv>
-                {productList.map(product =>
-                    <div key={product.id}>
-                        <Product product={product} />
-                    </div>
-                )}
-            </ItemListDiv>
-            <Pagination
-                total={total}
-                pageSize={9}
-                onPageChange={onPageChange}
-            />
-        </Div>
-        </MainDiv>
+        <ItemListDiv>
+          {productList.map((product) => (
+            <div key={product.id}>
+              <Product product={product} />
+            </div>
+          ))}
+        </ItemListDiv>
+        <Pagination total={total} pageSize={9} onPageChange={onPageChange} />
+      </Div>
+    </MainDiv>
   );
 }
 
 export default ProductList;
 
 export const CATEGORIES = {
-  0: '사료',
-  1: '간식',
-  2: '캔',
-  3: '모래',
+  0: "사료",
+  1: "간식",
+  2: "캔",
+  3: "모래",
 };
